dcef44c8d2118b692c8a2478052386ad628957bd
Michael Nebelsick <mnebelsick@comcast.net>
2017-05-22 16:26:32 -0500

    More files to support the Resume section I forgot to commit last time

---
+++ database/migrations/2017_05_22_192419_create_credentials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCredentialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('credentials', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type');
+            $table->string('image_class');
+            $table->string('timeframe');
+            $table->string('title');
+            $table->text('description');
+            $table->string('delay');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('credentials');
+    }
+}
